a97b61d7699a032c6c918cfa9183d247bb7cacd2
tokyoprikol <eugene19190@gmail.com>
2026-02-27 18:08:43 +0100

    create models for board, column and task. seed db with random data

---
+++ prisma/migrations/20260227163401_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Column" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "boardId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Column_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "position" TEXT NOT NULL DEFAULT 'a0',
+    "columnId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Column" ADD CONSTRAINT "Column_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_columnId_fkey" FOREIGN KEY ("columnId") REFERENCES "Column"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({
+  adapter,
+});
+
+async function seed() {
+  await prisma.board.create({
+    data: {
+      title: "My Board",
+      columns: {
+        create: [
+          {
+            title: "To Do",
+            order: 0,
+            tasks: {
+              create: [
+                {
+                  title: "learn Next.js",
+                  position: "a0",
+                },
+              ],
+            },
+          },
+          {
+            title: "In Progress",
+            order: 1,
+            tasks: {
+              create: [
+                {
+                  title: "learn React",
+                  position: "a1",
+                },
+              ],
+            },
+          },
+          {
+            title: "Done",
+            order: 2,
+            tasks: {
+              create: [
+                {
+                  title: "learn JavaScript",
+                  position: "a2",
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  });
+}
+
+seed();
